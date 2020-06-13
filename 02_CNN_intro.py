d14f94b1020136476cd17117895106a694b42dd1
Edoardo Carlesi <gatto@nanowar.it>
2020-06-13 16:31:08 +0200

    Check GPU

--- 02_CNN_intro.py
+++ 02_CNN_intro.py
@@ -13,6 +13,11 @@ from keras.layers import Dense
 
 from keras.preprocessing.image import ImageDataGenerator
 
+
+# Dataset path
+test_path = 'data/dataset/test_set/'
+train_path = 'data/dataset/train_set/'
+
 # Initialize the CNN
 classifier = Sequential()
 
@@ -47,12 +52,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('cnn_dataset/training_set',
+training_set = train_datagen.flow_from_directory(train_path, 
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('cnn_dataset/test_set',
+test_set = test_datagen.flow_from_directory(test_path, 
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
---
+++ check_gpu.py
@@ -0,0 +1,21 @@
+import tensorflow as tf 
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+from tensorflow.python.client import device_lib
+
+print(device_lib.list_local_devices())
+
+import keras
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
+
+config = tf.ConfigProto( device_count = {'GPU': 1 , 'CPU': 56} )
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
+
+print('TensorFlow Version: ')
+print(tf.__version__)
+
+print('Keras Version: ')
+print(keras.__version__)
