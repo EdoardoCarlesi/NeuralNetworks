67b8f983a5f7ec1c5a92d094b96ef1f9f0a7be3e
Edoardo Carlesi <gatto@nanowar.it>
2020-07-01 00:53:55 +0200

    Boltzmann machines

---
+++ 06_BoltzmannMachines.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel 
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+# Path and filenames
+path_dir = 'data/lm-ml/'
+path_movies = 'movies.dat'
+path_users = 'movies.dat'
+path_ratings = 'ratings.dat'
+
+# Import the dataset
+movies = pd.read_csv(path_dir + path_movies, sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+users = pd.read_csv(path_dir + path_users, sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+ratings = pd.read_csv(path_dir + path_ratings, sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+
